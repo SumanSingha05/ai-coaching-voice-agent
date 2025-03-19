662f7fa2f769126f21635467075c466945dc0f79
SumanSingha05 <sumansingha8654@gmail.com>
2025-03-19 19:06:22 +0530

    Shadcn installed, Button from Shadcn added.

--- app/page.js
+++ app/page.js
@@ -1,9 +1,12 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <h2>My Name is Suman Singha</h2>
+
+      <Button>Hello</Button>
     </div>
   );
 }
