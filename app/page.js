15d933f5991ccb07bdc0a378afbd41127d0b0e75
SumanSingha05 <sumansingha8654@gmail.com>
2025-03-19 19:19:21 +0530

    Authentication using stack auth done

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@stackframe/stack";
 import Image from "next/image";
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
       <h2>My Name is Suman Singha</h2>
 
       <Button>Hello</Button>
+      <UserButton />
     </div>
   );
 }
